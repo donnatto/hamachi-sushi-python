31c3c37c2117c405d2bcca3dfa5d8caf5983d7dd
Edwin Dominguez <edwindominguez@hotmail.com>
2019-12-05 17:41:55 -0500

    edit settings

--- hamachi/settings.py
+++ hamachi/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'neqmrv)_)$j=&(+9c652(!b=pt=)usp7o2337wgt2@ui1u22v!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
